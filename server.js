b54d75c424ae1f18e78c4c8743a669efd9501da7
Niranjan <niranjanbaviskar27@gmail.com>
2025-07-13 09:49:12 +0530

    original Server.js

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import billRoutes from "./routes/billRoutes.js";
-import runUpdateAdmin from "./updateAdmin.js";
-
-await runUpdateAdmin(); // <--- This runs first
 
 dotenv.config();
 const app = express();
